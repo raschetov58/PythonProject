12d608e771c68a3e81871e8727bf79f26b8d8178
raschetov58 <raschetov58@gmail.com>
2025-04-28 15:49:43 +0300

    7

--- test/test_status.py
+++ test/test_status.py
@@ -1,6 +1,6 @@
 import requests
 import pytest
 
-def test_piece_body ():
-    response = requests.get('https://belarusbank.by/api/kredits_info', params = {'type':'потребительский'})
-    assert response.status_code == 200
+def test_statur_code():
+    response = requests.get('https://belarusbank.by/api/kredits_info')
+    assert response.status_code == 400
