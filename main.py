b6215425e4e42819702cd4af6efce7943a4cbf30
raschetov58 <raschetov58@gmail.com>
2025-04-28 15:19:23 +0300

    3

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import requests
 import json
+
+
+response = requests.post('http://users.bugred.ru/tasks/rest/doregister', json={
+    "email": "odaisu@yandex.ru",
+    "name": "Саша",
+    "password": "1"
+})
+'''response_put = requests.put('http://users.bugred.ru/tasks/rest/addavatar', json={
+    "email": "odaisu@yandex.ru",
+    "name": "Саша",
+    "password": "1"
+})'''
+
+
+
+'''response = requests.get('https://belarusbank.by/api/kursExchange?city=Брест')'''
