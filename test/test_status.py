edf2ce508c0961498fee6c1f211c2ea42c75a1fa
raschetov58 <raschetov58@gmail.com>
2025-04-28 16:37:08 +0300

    8

--- test/test_status.py
+++ test/test_status.py
@@ -3,4 +3,9 @@ import pytest
 
 def test_statur_code():
     response = requests.get('https://belarusbank.by/api/kredits_info')
-    assert response.status_code == 400
+    assert response.status_code == 200
+    print(response.text)
+
+def test_piece_body ():
+    response = requests.get('https://belarusbank.by/api/kredits_info', params = {'kredit_type':'потребительский'})
+    assert response.json()['kredit_type'] == 'на недвижимость'
